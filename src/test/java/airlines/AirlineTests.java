d3b59882a2732dab562671b9aec5f638c549ef25
swapnil.pawar@gullak.money <swapnil.pawar@gullak.money>
2023-05-24 15:13:20 +0530

    running tests using testNG.xml & MavenRunner- completed

--- src/test/java/airlines/AirlineTests.java
+++ src/test/java/airlines/AirlineTests.java
@@ -1,4 +1,5 @@
 package airlines;
+import com.aventstack.extentreports.gherkin.model.ScenarioOutline;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -9,13 +10,16 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+// How to pass env in runtime?
+// edit configuration of mavenRunning with command "test -D env = qa"
+
 public class AirlineTests {
     @Test
     public void createAirline() throws IOException {
         String env = System.getProperty("env") == null ? "qa" : "dev";
         Map<String, String> data = jsonUtils.getJsonDataAsMap("airlines/"+env+"/airlinesApiData.json");
         String endPoint = data.get("createAirLineEndpoint");
-        Map<String, Object> payload = payloads.getCreateAirlinePayloadFromMap("98993", "Swapnil Airways", "IN", "ABC", "ABC Slogan", "Pune", "xyzz.com", "2023");
+        Map<String, Object> payload = payloads.getCreateAirlinePayloadFromMap("98905", "Swapnil Airways", "IN", "ABC", "ABC Slogan", "Pune", "xyzz.com", "2023");
         Response response = RestUtils.performPost(endPoint, payload, new HashMap<String, String>());
         Assert.assertEquals(response.statusCode(), 200);
     }
