4bbc80ba164931ab7373077ddf1498cb4a831b7e
swapnil.pawar@gullak.money <swapnil.pawar@gullak.money>
2023-06-02 15:54:31 +0530

    added datProvider annotation & methods, created new test using dataProvider

--- src/test/java/airlines/AirlineTests.java
+++ src/test/java/airlines/AirlineTests.java
@@ -8,10 +8,10 @@ import org.jvnet.staxex.StAxSOAPBody;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import utils.AssertionsUtils;
+import utils.excelUtils;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 // How to pass env in runtime?
 // edit configuration of mavenRunning with command "test -D env = qa"
@@ -78,6 +78,45 @@ public class AirlineTests extends AirlineAPIs{
         expectedValueMap.put("head_quaters", payload.getHead_quaters());
         expectedValueMap.put("website", payload.getWebsite());
         expectedValueMap.put("established", payload.getEstablished());
+        AssertionsUtils.assertExpectedValueWithJsonPath(response, expectedValueMap);
+    }
+
+
+    // dataProvider :- data driven approach
+        // step 1 - Create dataProvider
+    @DataProvider(name = "airlineData")
+    public Iterator<Airline> createAirlineData() throws IOException {
+        List<LinkedHashMap<String, String>> excelDataAsListMap =  excelUtils.getExcelDataListAsMap("createAirlineData", "Sheet1");
+        List<Airline> airlineData = new ArrayList<>();
+        for (LinkedHashMap<String, String> data : excelDataAsListMap) {
+            Airline airline = Airline.builder()
+                    .id(Integer.parseInt(data.get("id")))
+                    .name(data.get("name"))
+                    .country(data.get("country"))
+                    .logo(data.get("logo"))
+                    .slogan(data.get("slogan"))
+                    .head_quaters(data.get("headQuarter"))
+                    .website(data.get("website"))
+                    .established(data.get("established"))
+                    .build();
+            airlineData.add(airline);
+        }
+        return airlineData.iterator();
+    }
+
+    // step 2 = pass iterator from dataProvidor method to test method and fetch the value through it.
+    @Test(dataProvider = "airlineData")
+    public void createAirlineWithDataProvider(Airline airline){
+        Response response = createAirline(airline);
+        Map<String, Object> expectedValueMap = new HashMap<>();
+        expectedValueMap.put("id", airline.getId());
+        expectedValueMap.put("name", airline.getName());
+        expectedValueMap.put("country", airline.getCountry());
+        expectedValueMap.put("logo", airline.getLogo());
+        expectedValueMap.put("slogan", airline.getSlogan());
+        expectedValueMap.put("head_quaters", airline.getHead_quaters());
+        expectedValueMap.put("website", airline.getWebsite());
+        expectedValueMap.put("established", airline.getEstablished());
         AssertionsUtils.assertExpectedValueWithJsonPath(response, expectedValueMap);
     }
 
